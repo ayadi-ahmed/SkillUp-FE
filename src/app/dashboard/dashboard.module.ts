64754edeaef23c73e3b8cb094b2bd0069af94b27
Garaali-Marwen <garaali.marwen@gmail.com>
2023-03-19 03:48:35 +0100

    dashboard-sidenav + Profile centre

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,18 +6,39 @@ import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { BuyAbonnementComponent } from './buy-abonnement/buy-abonnement.component';
 import { MessagesComponent } from './messages/messages.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { ProfileComponent } from './Profile-Training-Center/profile/profile.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {HomeModule} from "../home/home.module";
+import { InstructorsComponent } from './Profile-Training-Center/instructors/instructors.component';
+import { CenterCoursesComponent } from './Profile-Training-Center/center-courses/center-courses.component';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    CartComponent,
-    CheckoutComponent,
-    BuyAbonnementComponent,
-    MessagesComponent
-  ],
+    declarations: [
+        DashboardComponent,
+        CartComponent,
+        CheckoutComponent,
+        BuyAbonnementComponent,
+        MessagesComponent,
+        ProfileComponent,
+        InstructorsComponent,
+        CenterCoursesComponent,
+    ],
+    exports: [
+        DashboardComponent
+    ],
     imports: [
         CommonModule,
-        DashboardRoutingModule
+        DashboardRoutingModule,
+        MatSidenavModule,
+        MatTabsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+        HomeModule
     ]
 })
 export class DashboardModule { }
